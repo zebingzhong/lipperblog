f01fef888b8004f1697c031851d3cbb9cdbedaeb
zzb <zhongzebing@1231.com>
2021-10-26 00:45:48 +0800

    wap

--- wap/src/views/first/index.vue
+++ wap/src/views/first/index.vue
@@ -1,17 +1,50 @@
 <template>
-  <div class="lipper-header">
-    <div class="lipper-header-content">
-      <div class="lipper-logo"></div>
-      <div class="lipper-nav">
-        <ul>
-          <li>首页</li>
-          <li>原创</li>
-          <li>分享</li>
-          <li>视频</li>
-          <li>资讯</li>
-        </ul>
+  <div class="box">
+    <div class="lipper-header">
+      <div class="lipper-header-content">
+        <div class="lipper-logo">
+          <img src="@/assets/images/logo.webp" alt="logo" />
+        </div>
+        <div class="lipper-nav">
+          <ul>
+            <li>首页</li>
+            <li>原创</li>
+            <li>分享</li>
+            <li>视频</li>
+            <li>资讯</li>
+          </ul>
+        </div>
+        <div class="lipper-search"></div>
       </div>
-      <div class="lipper-search"></div>
+
+    </div>
+    <div class="lipper-main">
+      <div class="lipper-banner">
+        <img src="@/assets/images/banner.jpeg" alt="">
+      </div>
+      <div class="lipper-content">
+        <div class="warp box-left">
+          <div class="lipper-item">
+            <div class="item-header">
+              <div class="item-title"></div>
+              <div class="item-label">
+                <div class="item-time"></div>
+              </div>
+            </div>
+            <div class="lipper-desc">
+              我是描述
+            </div>
+            <div class="item-cover">
+              <img src="" alt="">
+            </div>
+            <div class="item-bottom">
+              <div class="item-view">22</div>
+              <div class="item-comments">22</div>
+            </div>
+          </div>
+        </div>
+        <div class="warp box-right">3</div>
+      </div>
     </div>
   </div>
 </template>
