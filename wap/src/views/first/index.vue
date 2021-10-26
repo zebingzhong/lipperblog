dc6fa5b13455ed9030e7d58008e165ec57409bfc
zzb <zhongzebing@1231.com>
2021-10-27 01:07:21 +0800

    wap

--- wap/src/views/first/index.vue
+++ wap/src/views/first/index.vue
@@ -16,34 +16,74 @@
         </div>
         <div class="lipper-search"></div>
       </div>
-
     </div>
     <div class="lipper-main">
       <div class="lipper-banner">
-        <img src="@/assets/images/banner.jpeg" alt="">
+        <img src="@/assets/images/banner.jpeg" alt="" />
       </div>
       <div class="lipper-content">
         <div class="warp box-left">
           <div class="lipper-item">
             <div class="item-header">
-              <div class="item-title"></div>
-              <div class="item-label">
-                <div class="item-time"></div>
+              <img src="@/assets/images/avatar.webp" class="lipper-avatar" alt="" />
+              <div class="item-title">
+                <div class="item-title-tip">【中级一班】小姐姐美哦ddssdsdsad</div>
+                <div class="item-labels">
+                  <span class="item-label">@生活</span>
+                  <span class="item-time">一个月前</span>
+                </div>
               </div>
             </div>
             <div class="lipper-desc">
-              我是描述
+              孤独的行者，仰望星空，那么你看得到我吗？曾经对自己发誓，要看到天空的另外一边，似乎只有一个人才能透过黑暗，看到我的时候，一颗两颗。
+            </div>
+          </div>
+          <div class="lipper-item">
+            <div class="item-header">
+              <img src="@/assets/images/avatar.webp" class="lipper-avatar" alt="" />
+              <div class="item-title">
+                <div class="item-title-tip">【中级一班】小姐姐美哦ddssdsdsad</div>
+                <div class="item-labels">
+                  <span class="item-label">@生活</span>
+                  <span class="item-time">一个月前</span>
+                </div>
+              </div>
+            </div>
+            <div class="lipper-desc">
+              孤独的行者，仰望星空，那么你看得到我吗？
             </div>
             <div class="item-cover">
-              <img src="" alt="">
-            </div>
-            <div class="item-bottom">
-              <div class="item-view">22</div>
-              <div class="item-comments">22</div>
+              <img src="@/assets/images/img1.webp" alt="">
             </div>
           </div>
         </div>
-        <div class="warp box-right">3</div>
+        
+        <div class="warp box-right">
+          <div class="hot-box">
+            <div class="hot-tip">热门排行</div>
+            <div class="hot-item">
+              <img src="@/assets/images/img2.webp" alt="">
+              <div class="hot-content">
+                <div class="hot-title">网易云-API</div>
+                <div class="hot-view">428人围观</div>
+              </div>
+            </div>
+             <div class="hot-item">
+              <img src="@/assets/images/img2.webp" alt="">
+              <div class="hot-content">
+                <div class="hot-title">网易云-API</div>
+                <div class="hot-view">428人围观</div>
+              </div>
+            </div>
+             <div class="hot-item">
+              <img src="@/assets/images/img2.webp" alt="">
+              <div class="hot-content">
+                <div class="hot-title">网易云-API</div>
+                <div class="hot-view">428人围观</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
